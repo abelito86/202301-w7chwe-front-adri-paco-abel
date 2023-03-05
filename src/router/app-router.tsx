4ae1f9c1b5fb1bae580af829e83907d7dd241e48
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-05 15:23:19 +0100

    Add NotFound page to router, with tests and styles

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout/MainLayout';
 import Home from '../pages/Home/Home';
+import NotFound from '../pages/NotFound/NotFound';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    //errorElement: <NotFound/>
+    errorElement: <NotFound />,
     children: [
       {
         path: '',
