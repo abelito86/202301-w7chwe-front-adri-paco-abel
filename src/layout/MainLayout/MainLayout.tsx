6d1d84e9cda0edb188dd2fff2090977492070260
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-05 15:18:16 +0100

    Add root, app and main layout styles

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
-
+import Header from '../../features/Header/Header';
+import './MainLayout.css';
 const MainLayout = () => {
   return (
     <div className="app-container">
+      <Header />
       <main>
         <Outlet />
       </main>
